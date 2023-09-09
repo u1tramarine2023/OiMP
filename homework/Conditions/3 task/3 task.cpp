dc7a8a379fde7016589ed210fe2c1c9634259504
Marina <marinarina1406@gmail.com>
2023-09-09 23:28:43 +0300

    3 task completed

---
+++ homework/Conditions/3 task/3 task.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+int main()
+{
+	int a = 10;
+	int b = 5;
+	int c = 14;
+	int m1;
+	int m2;
+	if (a > b)
+	{
+		m1 = a; m2 = b;
+	}
+	else {
+		m1 = b; m2 = a;
+	}
+
+	if (c > m2) {
+		if (c > m1) {
+			m2 = m1;
+			m1 = c;
+		
+		}
+		else {
+			m2 = c;
+		}
+	}
+	std::cout << "Sum =" << m1 + m2;
+	
+	return 0;
+}
+
