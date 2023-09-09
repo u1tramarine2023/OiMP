ed5fac165c2613494c75b10b6faefc22d66d8637
Marina <marinarina1406@gmail.com>
2023-09-09 23:04:09 +0300

    2 task completed

---
+++ homework/1 task/2 task/2 task.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = 8;
+	std::cout << "starting order " << a <<" " << b <<" " << c << "\n";
+	int x;
+	if (a > b) x = a;
+	else x = b;
+	if (c > x) x = c;
+	std::cout << x;
+	
+	return 0;
+}
