845350d165720ac8e602ddc4106fffa05d4fb831
Marina <marinarina1406@gmail.com>
2023-09-09 22:42:23 +0300

    1 task complete

---
+++ homework/1 task/1 task/1 task.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+	int a = 4;
+	int b = 7;
+	if (a > b)
+	{
+		std::cout << "yes\n";
+	}
+	else
+	{
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+		std::cout << "a=" << a << " b=" << b;
+	}
+	return 0;
+}
